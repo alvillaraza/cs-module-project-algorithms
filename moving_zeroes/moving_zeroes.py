3969f757fbee6ed4a992ff9f0f751e481055e4dc
alexis villaraza <alexis.borja@gmail.com>
2020-06-17 15:47:50 -0700

    coded moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,17 +4,29 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-    #loop through and check if element == 0
-    for i in arr:
-        zero = []
+    _size = len(arr)
+#create a array of zeros
+    zero = []
+# create an array of numbers
+    numbers = []
+    for i in range(_size):
+        #**If number in the index equals 0...
         if arr[i] == 0:
-            zero.append(arr[i])
+            cur_index = arr[i]
+            # print('current element', arr[i])
+            #**put that number in the zero array
+            zero.append(cur_index)
+            # print('zero array = ', zero, 'end---')
 
-            print(zero)
-            
-    #if it's 0, move index +1
-
-    
+        #**If number in the index does not equal 0...
+        if arr[i] != 0:
+            cur_index = arr[i]
+            #**put that number in the numbers array
+            numbers.append(cur_index)
+            # print('numbers array=', numbers, 'end===')
+    # print(numbers + zero)
+#**combine the arrays together
+    return numbers + zero
 
 
 if __name__ == '__main__':
